5c59b25cd55cd93d3875cf0c7410a2a2b5b2e8e9
imgsrc <forifelse@yandex.ru>
2017-02-01 00:40:57 +0400

    Begin

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,30 @@
 $(function() {
+    //Toggle mnu
+    $('.toggle-mnu').on('click', function () {
+        $(this).toggleClass('on');
+        $(this).parent().next().next().find('.main-mnu').slideToggle();
+        return false;
+    });
 
-	// Custom JS
+    //E-mail Ajax Send
+    $('form').submit(function() { //Change
+        var th = $(this);
+        $.ajax({
+            type: 'POST',
+            url: 'mail.php', //Change
+            data: th.serialize()
+        }).done(function() {
+            $('.form-callback .success').addClass('active');
+            setTimeout(function() {
+                // Done Functions
+                $('.form-callback .success').removeClass('active');
+                th.trigger('reset');
+                $.magnificPopup.close();
+            }, 2000);
+        });
+        return false;
+    });
+
+
 
 });
