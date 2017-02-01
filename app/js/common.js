4be813498dc8fb5163ba412b0276ad0089b2cb22
imgsrc <forifelse@yandex.ru>
2017-02-02 00:01:00 +0400

    add 2 sect

--- app/js/common.js
+++ app/js/common.js
@@ -1,21 +1,15 @@
-$(function() {
-    //Toggle mnu
-    $('.toggle-mnu').on('click', function () {
-        $(this).toggleClass('on');
-        $(this).parent().next().next().find('.main-mnu').slideToggle();
-        return false;
-    });
+$(function () {
 
     //E-mail Ajax Send
-    $('form').submit(function() { //Change
+    $('form').submit(function () { //Change
         var th = $(this);
         $.ajax({
             type: 'POST',
             url: 'mail.php', //Change
             data: th.serialize()
-        }).done(function() {
+        }).done(function () {
             $('.form-callback .success').addClass('active');
-            setTimeout(function() {
+            setTimeout(function () {
                 // Done Functions
                 $('.form-callback .success').removeClass('active');
                 th.trigger('reset');
@@ -25,6 +19,20 @@ $(function() {
         return false;
     });
 
+    $('.s-adv').parallax();
+
+    var offerItem = $('.offer-item');
+    offerItem.each(function (e) {
+        var th = $(this);
+        th.attr('href', '#offer-' + e)
+            .find('.offer-popup')
+            .attr('id', 'offer-' + e);
+    });
+    offerItem.magnificPopup({
+        mainClass: 'my-mfp-zoom-in',
+        removalDelay: 300,
+        type: 'inline'
+    });
 
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -21,6 +21,8 @@ gulp.task('scripts', function () {
         'app/libs/jquery/dist/jquery.min.js',
         'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js',
         'app/libs/owl.carousel/dist/owl.carousel.min.js',
+        'app/libs/parallax.js/parallax.min.js',
+        'app/libs/wow/dist/wow.min.js',
         'app/js/common.js' // Всегда в конце
     ])
         .pipe(concat('scripts.min.js'))
